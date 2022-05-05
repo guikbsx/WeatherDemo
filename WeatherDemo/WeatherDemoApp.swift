05b04da4b4705e913fb8e7837ebc07cff46ccdb7
Guillaume Bisiaux <guillaume@heroes.jobs>
2022-05-05 09:48:01 +0200

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Guillaume Bisiaux on 05/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
